62078015ef207fba3c308eddc41c7868f7d25e26
Ivan Lepesii <lepesii.ivan@gmail.com>
2024-08-29 15:42:13 -0400

    Patient model - Closes #1

---
+++ Library.HospitalSystem/Models/Patient.cs
@@ -0,0 +1,38 @@
+﻿namespace Library.HospitalSystem.Models;
+
+public enum Race {
+    NotSpecified,
+    White,
+    Black,
+    AmericanIndianorAlaskaNative,
+    Asian,
+    NativeHawaiianorOtherPacificIslander,
+    Other
+}
+
+public enum Gender {
+    NotSpecified,
+    Male,
+    Female,
+    Other
+}
+public class Patient
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string adressLine1 { get; set; }
+    public string adressLine2 { get; set; }
+    public DateOnly DOB { get; set; }
+    public List<Race> Race { get; set; }
+    public Gender Gender { get; set; }
+
+    Patient() {
+        id = -1;
+        name = string.Empty;
+        adressLine1 = string.Empty;
+        adressLine2 = string.Empty;
+        DOB = DateOnly.FromDateTime(DateTime.UnixEpoch);
+        Race = new List<Race> { HospitalSystem.Models.Race.NotSpecified };
+        Gender = Gender.NotSpecified;
+    }
+}
