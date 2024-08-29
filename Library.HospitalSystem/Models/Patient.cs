9a28490741c83ea021a1dda26c8023aa566ee02e
Ivan Lepesii <lepesii.ivan@gmail.com>
2024-08-29 15:56:44 -0400

    Models: Make properties Pascal case

--- Library.HospitalSystem/Models/Patient.cs
+++ Library.HospitalSystem/Models/Patient.cs
@@ -18,19 +18,19 @@ public enum Gender {
 }
 public class Patient
 {
-    public int id { get; set; }
-    public string name { get; set; }
-    public string adressLine1 { get; set; }
-    public string adressLine2 { get; set; }
+    public uint Id { get; set; }
+    public string Name { get; set; }
+    public string AdressLine1 { get; set; }
+    public string AdressLine2 { get; set; }
     public DateOnly DOB { get; set; }
     public List<Race> Race { get; set; }
     public Gender Gender { get; set; }
 
     Patient() {
-        id = -1;
-        name = string.Empty;
-        adressLine1 = string.Empty;
-        adressLine2 = string.Empty;
+        Id = 0;
+        Name = string.Empty;
+        AdressLine1 = string.Empty;
+        AdressLine2 = string.Empty;
         DOB = DateOnly.FromDateTime(DateTime.UnixEpoch);
         Race = new List<Race> { HospitalSystem.Models.Race.NotSpecified };
         Gender = Gender.NotSpecified;
