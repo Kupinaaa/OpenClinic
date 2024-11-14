c738268264c3b3378d2cda694b98aa615aeec2f6
Ivan Lepesii <lepesii.ivan@gmail.com>
2024-11-13 20:04:37 -0500

    Frontend: Shadcn add table

--- next-frontend-hospital-system/src/app/admin/patients/page.tsx
+++ next-frontend-hospital-system/src/app/admin/patients/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 async function AdminPatients() {
     const data = await fetch("http://localhost:5222/api/patient");
     const patients = await data.json();
-    return <div className="">{JSON.stringify(patients)}</div>;
+    return <div className=""></div>;
 }
 
 export default AdminPatients;
