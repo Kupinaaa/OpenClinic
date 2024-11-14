41c05ff879b168f463c28483df608492e6ef69e0
Ivan Lepesii <lepesii.ivan@gmail.com>
2024-11-13 19:40:20 -0500

    Frontend: Fix tailwind import errors

--- next-frontend-hospital-system/src/app/admin/patients/page.tsx
+++ next-frontend-hospital-system/src/app/admin/patients/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 async function AdminPatients() {
     const data = await fetch("http://localhost:5222/api/patient");
     const patients = await data.json();
-    return <div>{JSON.stringify(patients)}</div>;
+    return <div className="">{JSON.stringify(patients)}</div>;
 }
 
 export default AdminPatients;
