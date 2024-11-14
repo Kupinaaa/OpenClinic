701c06e6fcc139284be4cdadb369af38ca4a9a21
Ivan Lepesii <lepesii.ivan@gmail.com>
2024-11-13 20:44:11 -0500

    Frontend: Fetch and display patients in a shadcn table, add PatientDTO type - Closes #99

--- next-frontend-hospital-system/src/app/admin/patients/page.tsx
+++ next-frontend-hospital-system/src/app/admin/patients/page.tsx
@@ -1,9 +1,45 @@
 import React from "react";
+import { PatientDTO } from "@/types/api/patient";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
 
 async function AdminPatients() {
     const data = await fetch("http://localhost:5222/api/patient");
-    const patients = await data.json();
-    return <div className=""></div>;
+    const patients = (await data.json()) as PatientDTO[];
+    // console.log(patients);
+    const patientComponents = patients.map((patient) => (
+        <TableRow key={patient.id}>
+            <TableCell>{patient.id}</TableCell>
+            <TableCell>{patient.name}</TableCell>
+            <TableCell>{patient.addressLine}</TableCell>
+            <TableCell>{patient.dob}</TableCell>
+            <TableCell>{patient.gender}</TableCell>
+            <TableCell>{patient.race}</TableCell>
+        </TableRow>
+    ));
+    return (
+        <Table>
+            <TableCaption>Patients</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead className="w-[100px]">Id</TableHead>
+                    <TableHead>Name</TableHead>
+                    <TableHead>Adress</TableHead>
+                    <TableHead>DOB</TableHead>
+                    <TableHead>Gender</TableHead>
+                    <TableHead>Race</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>{patientComponents}</TableBody>
+        </Table>
+    );
 }
 
 export default AdminPatients;
---
+++ next-frontend-hospital-system/src/types/api/patient.ts
@@ -0,0 +1,8 @@
+export interface PatientDTO {
+    id: number;
+    name: string;
+    addressLine: string;
+    dob: string;
+    gender: number;
+    race: number[];
+}
