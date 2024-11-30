e0995d4353aa0f84c8062b788319cb17a7144a47
Ivan Lepesii <lepesii.ivan@gmail.com>
2024-11-29 20:11:00 -0600

    Frontend: Basic display appiontment

--- next-frontend-hospital-system/src/app/admin/appointments/[id]/page.tsx
+++ next-frontend-hospital-system/src/app/admin/appointments/[id]/page.tsx
@@ -9,7 +9,39 @@ async function displayAppointment({
     const id = (await params).id;
     const data = await fetch(`http://localhost:5222/api/appointment/${id}`);
     const appointmentDto = (await data.json()) as AppointmentDto;
-    return <div>{JSON.stringify(appointmentDto)}</div>;
+    return (
+        <div className="h-full w-full p-20">
+            <h1 className="font-semibold text-5xl">
+                {appointmentDto.title}
+                <span className="text-gray-400 font-medium text-3xl">
+                    {" "}
+                    #{appointmentDto.id}
+                </span>
+            </h1>
+            <div className="w-full md:w-1/3 grid gap-4 pt-4 py-2">
+                <div className="grid grid-cols-4 items-center gap-4">
+                    <p className="text-gray-400">Description:</p>
+                    <p className="col-span-3">{appointmentDto.description}</p>
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                    <p className="text-gray-400">Start:</p>
+                    <p className="col-span-3">{appointmentDto.dateTimeStart}</p>
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                    <p className="text-gray-400">End:</p>
+                    <p className="col-span-3">{appointmentDto.dateTimeEnd}</p>
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                    <p className="text-gray-400">Patient Id:</p>
+                    <p className="col-span-3">{appointmentDto.patientId}</p>
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                    <p className="text-gray-400">Physician Id:</p>
+                    <p className="col-span-3">{appointmentDto.physicianId}</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default displayAppointment;
